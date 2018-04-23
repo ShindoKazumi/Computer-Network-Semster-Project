0ead7d9924f4fe5d5b07a2473fb26dd26e0f21b2
rxc102320 <rxc102320@utdallas.edu>
2018-04-23 18:19:19 -0500

    Update and rename Server.py to server.py
    Updated server to accept exit message from displayer in order to close current connection and go back to listening for incoming connections

---
+++ server.py
@@ -0,0 +1,34 @@
+import socket
+
+c = socket.socket()
+s = socket.socket()        
+host = socket.gethostname() 
+port = raw_input('Enter port number:')
+port = int(port)
+s.bind((host, port))
+
+s.listen(1)
+# Establish connection with controller
+contConn, addr = s.accept()
+# code to return list to controller
+
+contConn.close()
+while True:
+	while True:
+		# Establish connection with displayer
+		conn, addr = s.accept()     
+		print 'Got connection from', addr
+		print "Getting file name..."
+		name = conn.recv(1024)
+		if name == 'exit':
+			conn.close()
+			break
+		f = open(name,'rb')
+		l = f.read(1024)
+		while (l):
+			conn.send(l)
+			l = f.read(1024)
+		f.close()
+		print "Done Sending"
+		conn.shutdown(socket.SHUT_WR)
+		conn.close()
