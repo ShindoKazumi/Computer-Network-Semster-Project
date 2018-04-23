2aa3f8074b2fb4a4fce81ea9335afcc03be74fb6
rxc102320 <rxc102320@utdallas.edu>
2018-04-23 17:42:50 -0500

    Update and rename Client.py to client.py
    Updated to code to use pdftotext instead of PyPDF2 . Was having trouble reading test files using PyPDF2

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-import socket                                 # Import socket module
-import PyPDF2                                 # import PyPDF2         need to install into syatem using 'pip install PyPDF2'
-from getch import getch, pause                # import getch          need to install into system using 'pip install py-getch'
+import socket					# Import socket module
+import pdftotext				# import pdftotext		need to install into system using 'pip install pdftotext'
+from getch import getch, pause			# import getch			need to install into system using 'pip install py-getch'
 
 host = socket.gethostname()
 
@@ -25,16 +25,16 @@ while 1:
 		l = s.recv(1024)
 	f.close()
 	print "Done Receiving"
-	# creating a pdf file object
-	pdfFileObj = open('tempFile', 'rb')
 
-	# creating a pdf reader object
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pagenum = pdfReader.numPages
+	# creating a pdf file object
+	pdfile = open('tempFile', 'rb')
+	pdf = pdftotext.PDF(pdfile)
+
+	pagenum = len(pdf)	
 	page = 0
 	pagenum = int(pagenum)
 	page = int(page)
-	# creating a page object
+
 	print 'Press the A and D keys to move through the file'
 	print 'Or press Q to quit'
 
@@ -44,15 +44,12 @@ while 1:
 			if 0 < page:  
 				page = page - 1
 		elif (input == 'D') or (input == 'd'):
-			if page < pagenum:
+			if page < pagenum-1:
 				page = page + 1
+			else:
+				break
 		elif (input == 'Q') or (input == 'q'):
 			break
-		pageObj = pdfReader.getPage(page)
-		print(pageObj.extractText())
- 
-	# closing the pdf file object
-	pdfFileObj.close()
-  pdfReader.stream.close()
+		print(pdf[page])
 # Close the socket when done
-s.close()                     
+s.close()                                   
