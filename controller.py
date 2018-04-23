a3b50e9edc29d72c93aa6ecec3e5fcff1e06d830
rxc102320 <rxc102320@utdallas.edu>
2018-04-23 15:56:01 -0500

    Add files via upload
    controller skeleton. Needs functionality

---
+++ controller.py
@@ -0,0 +1,28 @@
+import socket
+
+host = socket.gethostname()
+
+# connect controller to server
+s = socket.socket()
+sPort = raw_input('Enter server port number: ')
+sPort = int(sPort)
+s.connect((host, sPort))
+
+# set up displayer connection
+c = socket.socket()
+cPort = raw_input('Enter controller port number: ')
+cPort = int(cPort)
+c.bind((host, cPort)) 
+c.listen(1)
+
+while True:
+	# Establish connection with displayer
+	conn, addr = c.accept()
+	while True:
+		# code to request list from server and then send to displayer
+		print 'Connection address:', addr
+		break
+	# close connection with displayer
+	conn.close()	
+	s.close()
+	c.close()
