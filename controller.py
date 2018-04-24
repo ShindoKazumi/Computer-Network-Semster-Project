9e93b3b246e08e3634c7f952048062df98d05829
rxc102320 <rxc102320@utdallas.edu>
2018-04-24 14:51:20 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,26 +3,29 @@ import socket
 host = socket.gethostname()
 
 # connect controller to server
-s = socket.socket()
-sPort = raw_input('Enter server port number: ')
-sPort = int(sPort)
-s.connect((host, sPort))
-
+#sPort = raw_input('Enter server port number: ')
+#sPort = int(sPort)
+sPort = 9999
+cPort = 9998
 # set up displayer connection
 c = socket.socket()
-cPort = raw_input('Enter controller port number: ')
-cPort = int(cPort)
+#cPort = raw_input('Enter controller port number: ')
+#cPort = int(cPort)
 c.bind((host, cPort)) 
 c.listen(1)
 
 while True:
 	# Establish connection with displayer
+	s = socket.socket()
+	s.connect((host, sPort))	
 	conn, addr = c.accept()
 	while True:
 		# code to request list from server and then send to displayer
-		cmd = raw_input("user > ")
+		print 'Connection address:', addr		
+		cmd = 'ls files'
 		s.send(cmd)
-		print 'Connection address:', addr
+		output = s.recv(1024)
+		conn.send(output)		
 		break
 	# close connection with displayer
 	conn.close()
